c80d24116ec9e5006bc4ff987e8f0307fc459fac
queue <queue@hakase.org>
2014-03-15 09:25:00 +0400

    Timestamp scripts when fetching
    cloudflare caches the shit out of those things,
    probably missed some updates. whoops.

--- update.sh
+++ update.sh
@@ -9,21 +9,21 @@ git pull origin master
 EXTENSION=$(curl \
   --fail \
   --user-agent "4chan-JS-auto-updater/1.0.0" \
-  "http://s.4cdn.org/js/extension.js")
+  "http://s.4cdn.org/js/extension.$(date +%s).js")
 
 js-beautify --indent-size=2 - <<< "$EXTENSION" > extension.js
 
 CORE=$(curl \
   --fail \
   --user-agent "4chan-JS-auto-updater/1.0.0" \
-  "http://s.4cdn.org/js/core.js")
+  "http://s.4cdn.org/js/core.$(date +%s).js")
 
 js-beautify --indent-size=2 - <<< "$CORE" > core.js
 
 CATALOG=$(curl \
   --fail \
   --user-agent "4chan-JS-auto-updater/1.0.0" \
-  "http://s.4cdn.org/js/catalog.js")
+  "http://s.4cdn.org/js/catalog.$(date +%s).js")
 
 js-beautify --indent-size=2 - <<< "$CATALOG" > catalog.js
 
