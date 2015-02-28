68189ce1307eac37a8e6bc34551a502241d6eb7d
Anonymous ## Developer <sage@4chan.org>
2015-02-28 19:33:12 +0000

    Update catalog
    (update detected by update.sh)

--- catalog-compiled.js
+++ catalog-compiled.js
@@ -2657,7 +2657,7 @@ CustomMenu.apply = function(str) {
 
   cnt.appendChild(document.createTextNode(' ['));
   el = document.createElement('a');
-  el.textContent = '&hellip;';
+  el.textContent = '…';
   el.title = 'Show all';
   el.className = 'show-all-boards pointer';
   cnt.appendChild(el);
